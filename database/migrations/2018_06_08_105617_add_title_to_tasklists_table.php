9189941ad472f53dd24791d9d48fb8b12224081c
hisashi.maedan <ec2-user@ip-172-31-32-120.us-east-2.compute.internal>
2018-06-08 01:58:28 +0000

    add status

--- database/migrations/2018_06_08_105617_add_title_to_tasklists_table.php
+++ database/migrations/2018_06_08_105617_add_title_to_tasklists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddstatusToTasklistsTable extends Migration
+class AddTitleToTasklistsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -18,11 +18,15 @@ class AddstatusToTasklistsTable extends Migration
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::table('tasklists', function (Blueprint $table) {
             $table->dropColumn('status');
-            //
         });
     }
 }
